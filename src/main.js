58fca7c384d7968167ce9360c7b280e311731c42
Randy Miller <randy918@gmail.com>
2024-08-16 17:56:40 -0500

    have the js import list good  8-16-24-1756

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import '../styles/modern-normalize.css';
+import '../styles/components/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
+import '../styles/components/about.css';
 import '../styles/utils.css';
